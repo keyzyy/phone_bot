f3904fd19043babf373ab36a1fa89241155adae5
Bakhodir <bahodirkamilov054@gmail.com>
2026-04-14 21:20:34 +0500

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ import sqlite3
 conn = sqlite3.connect("shop.db", check_same_thread=False)
 cursor = conn.cursor()
 
-# table yaratish
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS orders (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
