e9e55ee70f66e180cc92022f60b307df4bb71722
Bakhodir <bahodirkamilov054@gmail.com>
2026-04-14 21:35:57 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+# DATABASE ulash
 conn = sqlite3.connect("shop.db", check_same_thread=False)
 cursor = conn.cursor()
 
+# TABLE yaratish (agar yo‘q bo‘lsa)
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS orders (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,9 +16,15 @@ CREATE TABLE IF NOT EXISTS orders (
 
 conn.commit()
 
+# BUYURTMA SAQLASH
 def save_order(name, phone, product):
     cursor.execute(
         "INSERT INTO orders (name, phone, product) VALUES (?, ?, ?)",
         (name, phone, product)
     )
     conn.commit()
+
+# HAMMA BUYURTMANI OLISH (ADMIN uchun)
+def get_orders():
+    cursor.execute("SELECT * FROM orders ORDER BY id DESC")
+    return cursor.fetchall()
